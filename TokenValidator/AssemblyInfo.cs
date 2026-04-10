38785f9f3a9d978e1691af9ce98d2114a4464602
Miller <72067078+MillerJrxd@users.noreply.github.com>
2026-04-10 23:44:17 +0200

    Update assembly version to 2.4

--- TokenValidator/AssemblyInfo.cs
+++ TokenValidator/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Windows;
                                                 // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.3.*")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
-[assembly: AssemblyInformationalVersion("2.3.1")]
+[assembly: AssemblyVersion("2.4.*")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
